368fad548ab506a7965eb68d562d54c15696b07a
Honey <honiism@gmail.com>
2022-02-14 21:52:19 +0700

    Added license headers

--- src/main/java/com/honiism/discord/lemi/utils/customEmbeds/EmbedTools.java
+++ src/main/java/com/honiism/discord/lemi/utils/customEmbeds/EmbedTools.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2022 Honiism
+ * 
+ * This file is part of Lemi-Bot.
+ * 
+ * Lemi-Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Lemi-Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Lemi-Bot. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.honiism.discord.lemi.utils.customEmbeds;
 
 import java.util.HashMap;
@@ -15,11 +34,11 @@ import net.dv8tion.jda.api.interactions.InteractionHook;
 
 public class EmbedTools {
 
-    private IEmbedSync embedListener;
+    private IEmbedListener embedListener;
     private Map<String, String> embedProperties = new HashMap<String, String>();
     private Map<String, Integer> embedColor = new HashMap<String, Integer>();
   
-    public void registerEmbedListener(IEmbedSync embedListener) {
+    public void registerEmbedListener(IEmbedListener embedListener) {
         this.embedListener = embedListener;
     }
 
