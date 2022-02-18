358bd167e36274b78e794bd931c29b85737dfacb
illusbyhoppity <Amanda141107@gmail.com>
2022-02-18 23:28:41 +0700

    Changed variable name

--- src/main/java/com/honiism/discord/lemi/Config.java
+++ src/main/java/com/honiism/discord/lemi/Config.java
@@ -22,9 +22,9 @@ package com.honiism.discord.lemi;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
-    private static final Dotenv dotenv = Dotenv.load();
+    private static final Dotenv DOTENV = Dotenv.load();
 
     public static String get(String key) {
-        return dotenv.get(key.toUpperCase());
+        return DOTENV.get(key.toUpperCase());
     }
 }
