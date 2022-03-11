a591b0a873b880efd137a62bb82a0e3a5521a4f2
illusbyhoppity <honiism@gmail.com>
2022-03-11 23:21:14 +0700

    Added UserData

---
+++ src/main/java/com/honiism/discord/lemi/data/UserData.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (C) 2022 Honiism
+ * 
+ * This file is part of Lemi-Bot.
+ * 
+ * Lemi-Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Lemi-Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Lemi-Bot. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.honiism.discord.lemi.data;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UserData {
+
+    @JsonProperty("id")
+    private final long id;
+
+    @JsonProperty("balance")
+    private long balance;
+    @JsonProperty("deaths")
+    private long deaths;
+
+    @JsonProperty("passive_mode")
+    private boolean passiveMode;
+    @JsonProperty("inventory")
+    private List<InventoryData> InventoryData;
+
+    public UserData(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public long getdeaths() {
+        return deaths;
+    }
+
+    public boolean getPassiveMode() {
+        return passiveMode;
+    }
+
+    public List<InventoryData> getInventory() {
+        return InventoryData;
+    }
+
+    public void setBalance(long balance) {
+        if (balance < 1000) {
+            balance = 1000;
+        }
+
+        this.balance = balance;
+    }
+
+    public void setdeaths(long deaths) {
+        this.deaths = deaths;
+    }
+
+    public void setPassiveMode(boolean passiveMode) {
+        this.passiveMode = passiveMode;
+    }
+
+    public void setInventory(List<InventoryData> InventoryData) {
+        this.InventoryData = InventoryData;
+    }
+
+    public class InventoryData {
+        
+        @JsonProperty("id")
+        private final String id;
+
+        @JsonProperty("name")
+        private String name;
+        @JsonProperty("count")
+        private long count;
+
+        public InventoryData(String id) {
+            this.id = id;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public long getCount() {
+            return count;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+            
+        public void setCount(long count) {
+            this.count = count;
+        }
+    }
+}
