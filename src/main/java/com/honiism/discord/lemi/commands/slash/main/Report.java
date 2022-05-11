7b593a46af54f7550528799647b9a774d64abde5
illusbyhoppity <honiism@gmail.com>
2022-05-12 02:26:49 +0700

    Updated JDA version

--- src/main/java/com/honiism/discord/lemi/commands/slash/main/Report.java
+++ src/main/java/com/honiism/discord/lemi/commands/slash/main/Report.java
@@ -41,7 +41,7 @@ import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
-import net.dv8tion.jda.api.interactions.components.text.Modal;
+import net.dv8tion.jda.api.interactions.components.Modal;
 import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 
