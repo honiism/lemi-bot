39cf387eced594c5e7a11c7ad728e885b9db0e98
illusbyhoppity <honiism@gmail.com>
2022-05-12 20:15:12 +0700

    Rewriting staff slash commands as text commands

--- src/main/java/com/honiism/discord/lemi/commands/text/staff/dev/Compile.java
+++ src/main/java/com/honiism/discord/lemi/commands/text/staff/dev/Compile.java
@@ -28,13 +28,15 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import com.honiism.discord.lemi.Config;
 import com.honiism.discord.lemi.Lemi;
 import com.honiism.discord.lemi.commands.handler.CommandCategory;
 import com.honiism.discord.lemi.commands.handler.UserCategory;
-import com.honiism.discord.lemi.commands.slash.handler.SlashCmd;
+import com.honiism.discord.lemi.commands.text.handler.CommandContext;
+import com.honiism.discord.lemi.commands.text.handler.TextCmd;
 import com.honiism.discord.lemi.utils.misc.Tools;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 
@@ -46,38 +48,28 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.interactions.InteractionHook;
-import net.dv8tion.jda.api.interactions.commands.OptionMapping;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
-public class Compile extends SlashCmd {
+public class Compile extends TextCmd {
 
     private static final Logger log = LoggerFactory.getLogger(Compile.class);
     private HashMap<Long, Long> delay = new HashMap<>();
     private long timeDelayed;
 
     public Compile() {
-        setCommandData(Commands.slash("compile", "Compile code using Lemi's core system.")
-                .addOption(OptionType.STRING, "language", "Language you want to use to code.", true)
-                .addOption(OptionType.STRING, "version_index", "The version index of the language to use.", true)
-                .addOption(OptionType.STRING, "input", "Input for the code if needed for execution.", false)
-        );
-
-        setUsage("/dev compile <language> <version_index> [input]");
+        setName("compile");
+        setDesc("Compile code using Lemi's core system.");
+        setUsage("compile <language> <version_index> [input]");
         setCategory(CommandCategory.DEV);
         setUserCategory(UserCategory.DEV);
         setUserPerms(new Permission[] {Permission.ADMINISTRATOR});
         setBotPerms(new Permission[] {Permission.ADMINISTRATOR});
-        
     }
 
     @Override
-    public void action(SlashCommandInteractionEvent event) {
-        InteractionHook hook = event.getHook();
-        User author = event.getUser();
+    public void action(CommandContext ctx) {
+        MessageReceivedEvent event = ctx.getEvent();
+        User author = event.getAuthor();
 
         if (delay.containsKey(author.getIdLong())) {
             timeDelayed = System.currentTimeMillis() - delay.get(author.getIdLong());
@@ -92,16 +84,26 @@ public class Compile extends SlashCmd {
         
             delay.put(author.getIdLong(), System.currentTimeMillis());
 
-            hook.sendMessage(":coconut: Please type in your code in 1 minute!").queue();
+            List<String> args = ctx.getArgs();
 
-            String inputOption = event.getOption("input", OptionMapping::getAsString);
-            String versionIndex = event.getOption("version_index", OptionMapping::getAsString);
-            String language = event.getOption("language", OptionMapping::getAsString);
+            if (args.isEmpty() || args.size() < 2) {
+                event.getMessage().replyEmbeds(getHelp(event)).content(":milk: Usage: `" + getUsage() + "`!").queue();
+                return;
+            }
 
-            waitAndCompile(Lemi.getInstance().getEventWaiter(), hook,
-                    (inputOption != null) ? inputOption : null,
-                    language, versionIndex);
-                    
+            event.getMessage().reply(":coconut: Please type in your code in 1 minute!").queue((msg) -> {
+                String language = args.get(0);
+                String versionIndex = args.get(1);
+
+                if (args.size() > 2) {
+                    String inputOption = args.get(2);
+
+                    waitAndCompile(Lemi.getInstance().getEventWaiter(), event, inputOption, language, versionIndex);
+                } else {
+                    waitAndCompile(Lemi.getInstance().getEventWaiter(), event, null, language, versionIndex);   
+                }
+            });
+
         } else {
             String time = Tools.secondsToTime(((5 * 1000) - timeDelayed) / 1000);
                 
@@ -112,75 +114,76 @@ public class Compile extends SlashCmd {
                         + ", you can use this command again in `" + time + "`.")
                 .setColor(0xffd1dc);
                 
-            hook.sendMessageEmbeds(cooldownMsgEmbed.build()).queue();
+            event.getMessage().replyEmbeds(cooldownMsgEmbed.build()).queue();
         }
     }
 
-    private void waitAndCompile(EventWaiter waiter, InteractionHook hook, String input, String language, String verIndex) {
-        Member member = hook.getInteraction().getMember();
+    private void waitAndCompile(EventWaiter waiter, MessageReceivedEvent event, String input, String language, String verIndex) {
+        Member member = event.getMember();
 
         waiter.waitForEvent(
                 MessageReceivedEvent.class,
 
-                (event) -> event.getAuthor().getIdLong() == member.getIdLong()
-                        && event.isFromGuild()
-                        && event.getGuild().getIdLong() == Config.getLong("honeys_hive"),
+                (e) -> e.getAuthor().getIdLong() == member.getIdLong()
+                        && e.isFromGuild()
+                        && e.getGuild().getIdLong() == event.getGuild().getIdLong()
+                        && !e.getAuthor().isBot(),
 
-                (event) -> {
-                    hook.editOriginal("Your code is being compiled, please wait... :coffee:").queue();
-
-                    String script = event.getMessage().getContentRaw().replaceAll("```", "");
+                (e) -> {
+                    e.getMessage().reply("Your code is being compiled, please wait... :coffee:").queue((msg) -> {
+                        String script = e.getMessage().getContentRaw().replaceAll("```", "");
                     
-                    JSONObject bodyReq = new JSONObject()
-                        .put("clientId", Config.get("jdoodle_client_id"))
-                        .put("clientSecret", Config.get("jdoodle_client_secret"))
-                        .put("script", script)
-                        .put("language", language)
-                        .put("versionIndex", verIndex);
+                        JSONObject bodyReq = new JSONObject()
+                            .put("clientId", Config.get("jdoodle_client_id"))
+                            .put("clientSecret", Config.get("jdoodle_client_secret"))
+                            .put("script", script)
+                            .put("language", language)
+                            .put("versionIndex", verIndex);
 
-                    if (input != null) {
-                        bodyReq.put("stdin", input);
-                    }
+                        if (input != null) {
+                            bodyReq.put("stdin", input);
+                        }
 
-                    HttpClient httpClient = HttpClient.newBuilder()
-                        .version(Version.HTTP_2)
-                        .followRedirects(Redirect.NORMAL)
-                        .connectTimeout(Duration.ofSeconds(10))
-                        .build();
+                        HttpClient httpClient = HttpClient.newBuilder()
+                            .version(Version.HTTP_2)
+                            .followRedirects(Redirect.NORMAL)
+                            .connectTimeout(Duration.ofSeconds(10))
+                            .build();
 
-                    HttpRequest request = HttpRequest.newBuilder()
-                        .POST(HttpRequest.BodyPublishers.ofString(bodyReq.toString()))
-                        .uri(URI.create("https://api.jdoodle.com/v1/execute"))
-                        .setHeader("User-Agent", Config.get("user_agent"))
-                        .header("Content-Type", Config.get("content_type"))
-                        .build();
+                        HttpRequest request = HttpRequest.newBuilder()
+                            .POST(HttpRequest.BodyPublishers.ofString(bodyReq.toString()))
+                            .uri(URI.create("https://api.jdoodle.com/v1/execute"))
+                            .setHeader("User-Agent", Config.get("user_agent"))
+                            .header("Content-Type", Config.get("content_type"))
+                            .build();
 
-                    try {
-                        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-                        JSONObject jsonRespose = new JSONObject(response.body());
+                        try {
+                            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+                            JSONObject jsonRespose = new JSONObject(response.body());
 
-                        String output = jsonRespose.getString("output");
-                        int statusCode = jsonRespose.getInt("statusCode");
+                            String output = jsonRespose.getString("output");
+                            int statusCode = jsonRespose.getInt("statusCode");
 
-                        EmbedBuilder resultEmbed = new EmbedBuilder()
-                            .setTitle("‧₊੭ :tulip: Here is your code output!\r\n"
-                                + "๑‧˚₊꒷꒦︶︶︶︶︶꒷꒦︶︶︶︶︶✦‧₊˚⊹")
-                            .setAuthor(member.getUser().getAsTag(), null, member.getUser().getEffectiveAvatarUrl())
-                            .setColor(0xffd1dc)
-                            .addField("❥ **REQUESTED BY** :sunflower:", member.getAsMention(), false)
-                            .addField("❥ **STATUS CODE** :seedling:", String.valueOf(statusCode) , false)
-                            .setThumbnail(event.getGuild().getSelfMember().getUser().getEffectiveAvatarUrl())
-                            .setDescription("```" + output + "```");
+                            EmbedBuilder resultEmbed = new EmbedBuilder()
+                                .setTitle("‧₊੭ :tulip: Here is your code output!\r\n"
+                                        + "๑‧˚₊꒷꒦︶︶︶︶︶꒷꒦︶︶︶︶︶✦‧₊˚⊹")
+                                .setAuthor(member.getUser().getAsTag(), null, member.getUser().getEffectiveAvatarUrl())
+                                .setColor(0xffd1dc)
+                                .addField("❥ **REQUESTED BY** :sunflower:", member.getAsMention(), false)
+                                .addField("❥ **STATUS CODE** :seedling:", String.valueOf(statusCode) , false)
+                                .setThumbnail(e.getGuild().getSelfMember().getUser().getEffectiveAvatarUrl())
+                                .setDescription("```" + output + "```");
 
-                        hook.editOriginal("Here's your result! :cherries:").setEmbeds(resultEmbed.build()).queue();
+                            msg.editMessage("Here's your result! :cherries:").setEmbeds(resultEmbed.build()).queue();
 
-                    } catch (IOException | InterruptedException e) {
-                        Tools.sendError("get the code output", "IOException", log, hook, e);
-                    }
+                        } catch (IOException | InterruptedException ex) {
+                            Tools.sendError("get the code output", "IOException", log, event.getMessage(), ex);
+                        }
+                    });
                 },
                 1L, TimeUnit.MINUTES,
                 () -> {
-                    hook.editOriginal("Operation cancelled due to timeout! :cloud:").queue();
+                    event.getMessage().reply("Operation cancelled due to timeout! :cloud:").queue();
                 }
         );
     }
