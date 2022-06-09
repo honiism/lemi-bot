e2ef9f18b4be8b7f1a9cacf3b7a4837b4e2cf741
illusbyhoppity <honiism@gmail.com>
2022-06-09 09:11:46 +0700

    Added dare, wyr, nhie, paranoia commands

--- src/main/java/com/honiism/discord/lemi/commands/slash/fun/TruthOrDare.java
+++ src/main/java/com/honiism/discord/lemi/commands/slash/fun/TruthOrDare.java
@@ -29,12 +29,14 @@ import java.net.http.HttpClient.Redirect;
 import java.net.http.HttpClient.Version;
 import java.time.Duration;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.http.client.utils.URIBuilder;
 
 import com.google.gwt.thirdparty.json.JSONException;
 import com.google.gwt.thirdparty.json.JSONObject;
 import com.honiism.discord.lemi.Config;
+import com.honiism.discord.lemi.Lemi;
 import com.honiism.discord.lemi.commands.handler.CommandCategory;
 import com.honiism.discord.lemi.commands.handler.UserCategory;
 import com.honiism.discord.lemi.commands.slash.handler.AutocompleteChoices;
@@ -42,9 +44,12 @@ import com.honiism.discord.lemi.commands.slash.handler.SlashCmd;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
@@ -59,6 +64,23 @@ public class TruthOrDare extends SlashCmd {
         setCommandData(Commands.slash("tod", "A group of ToD commands.")
                 .addSubcommands(
                         new SubcommandData("truth", "Get a truth question (default: PG13).")
+                                .addOption(OptionType.STRING, "rating", "The maturity rating.",
+                                        false, true),
+
+                        new SubcommandData("dare", "Get a dare question (default: PG13).")
+                                .addOption(OptionType.STRING, "rating", "The maturity rating.",
+                                        false, true),
+
+                        new SubcommandData("wyr", "Get a Would You Rather question (default: PG13).")
+                                .addOption(OptionType.STRING, "rating", "The maturity rating.",
+                                        false, true),
+
+                        new SubcommandData("nhie", "Get a Never Have I Ever question (default: PG13).")
+                                .addOption(OptionType.STRING, "rating", "The maturity rating.",
+                                        false, true),
+
+                        new SubcommandData("paranoia", "Get a paranoia question (default: PG13).")
+                                .addOption(OptionType.USER, "user", "User to ask.", false)
                                 .addOption(OptionType.STRING, "rating", "The maturity rating.",
                                         false, true)
                 )
@@ -75,11 +97,99 @@ public class TruthOrDare extends SlashCmd {
     public void action(SlashCommandInteractionEvent event) {
         String subCmdName = event.getSubcommandName();
         InteractionHook hook = event.getHook();
+
+        String rating = event.getOption("rating", "PG13", OptionMapping::getAsString);
+
+        if (!rating.equals("PG") && !rating.equals("PG13") && !rating.equals("R")) {
+            hook.sendMessage(":snowflake: Invalid rating. Available ones: PG, PG13, R.").queue();
+            return;
+        }
+
+        JSONObject jsonRespose = handleRequest(rating, subCmdName, hook);
         
         switch (subCmdName) {
             case "truth":
-                String rating = event.getOption("rating", "PG13", OptionMapping::getAsString);
-                handleRequest(rating, "truth", hook);
+            case "dare":
+            case "wyr":
+            case "nhie":
+                try {
+                    String question = jsonRespose.getString("question");
+                    String id = jsonRespose.getString("id");
+
+                    Member member = hook.getInteraction().getMember();
+
+                    EmbedBuilder questionEmbed = new EmbedBuilder()
+                        .setTitle(":tulip: " + subCmdName.toUpperCase() + " question!")
+                        .setDescription(question)
+                        .setAuthor(member.getEffectiveName(), null, member.getEffectiveAvatarUrl())
+                        .setFooter("ID: " + id + " | Rating: " + rating)
+                        .setThumbnail(hook.getInteraction().getGuild().getSelfMember().getEffectiveAvatarUrl())
+                        .setColor(0xffd1dc);
+
+                    hook.sendMessageEmbeds(questionEmbed.build()).queue();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case "paranoia":
+                try {
+                    String question = jsonRespose.getString("question");
+                    String id = jsonRespose.getString("id");
+
+                    Member member = event.getOption("user", event.getMember(), OptionMapping::getAsMember);
+
+                    EmbedBuilder questionEmbed = new EmbedBuilder()
+                        .setTitle(":tulip: " + subCmdName.toUpperCase() + " question!")
+                        .setDescription(question)
+                        .setAuthor(member.getEffectiveName(), null, member.getEffectiveAvatarUrl())
+                        .setFooter("ID: " + id + " | Rating: " + rating + " | Type: " + subCmdName)
+                        .setThumbnail(hook.getInteraction().getGuild().getSelfMember().getEffectiveAvatarUrl())
+                        .setColor(0xffd1dc);
+
+                    member.getUser().openPrivateChannel().queue(
+                        (channel) -> {
+                            hook.sendMessage(":strawberry: Sent them the question.").queue();
+
+                            channel.sendMessage(":grapes: Please respond with an answer in 5 minutes.")
+                                .setEmbeds(questionEmbed.build())
+                                .queue((msg) -> {
+                                    Lemi.getInstance().getEventWaiter().waitForEvent(
+                                            MessageReceivedEvent.class,
+                                            (e) -> e.isFromType(ChannelType.PRIVATE)
+                                                && e.getAuthor().getIdLong() == member.getIdLong()
+                                                && !e.getAuthor().isBot(),
+                                            (e) -> {
+                                                TextChannel textChannel = hook.getInteraction().getTextChannel();
+                                                
+                                                EmbedBuilder answerEmbed = new EmbedBuilder()
+                                                    .setTitle(":strawberry: Answer Received!")
+                                                    .addField(":sunflower: Question", question, false)
+                                                    .addField(":seedling: Answer", e.getMessage().getContentDisplay(), false)
+                                                    .setAuthor(member.getEffectiveName(), null, member.getEffectiveAvatarUrl())
+                                                    .setFooter("ID: " + id + " | Rating: " + rating + " | Type: " + subCmdName)
+                                                    .setThumbnail(hook.getInteraction().getGuild().getSelfMember().getEffectiveAvatarUrl())
+                                                    .setColor(0xffd1dc);
+
+                                                textChannel.sendMessage(member.getAsMention() + "has responded!")
+                                                    .setEmbeds(answerEmbed.build())
+                                                    .queue((message) -> {
+                                                        channel.sendMessage("Answer sent.").queue();
+                                                    });
+                                            },
+                                            5,
+                                            TimeUnit.MINUTES,
+                                            null
+                                    );
+                                });
+                        },
+                        (error) -> {
+                            hook.sendMessage(":grapes: Cannot dm that user.").queue();
+                        }
+                    );
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
         }
     }
 
@@ -96,7 +206,7 @@ public class TruthOrDare extends SlashCmd {
         ).queue();
     }
 
-    private void handleRequest(String rating, String reqType, InteractionHook hook) {
+    private JSONObject handleRequest(String rating, String reqType, InteractionHook hook) {
         try {
             HttpClient httpClient = HttpClient.newBuilder()
                 .version(Version.HTTP_2)
@@ -115,22 +225,12 @@ public class TruthOrDare extends SlashCmd {
                 .build();
 
             HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            JSONObject jsonRespose = new JSONObject(response.body());
-
-            String question = jsonRespose.getString("question");
-            String id = jsonRespose.getString("id");
-
-            Member member = hook.getInteraction().getMember();
-
-            EmbedBuilder questionEmbed = new EmbedBuilder()
-                .setTitle(":tulip: " + reqType.toUpperCase() + " question!")
-                .setDescription(question)
-                .setAuthor(member.getEffectiveName(), null, member.getEffectiveAvatarUrl())
-                .setFooter("Type: " + reqType + " | id: " + id + " | rating: " + rating);
-
-            hook.sendMessageEmbeds(questionEmbed.build()).queue();
+            
+            return new JSONObject(response.body());
         } catch (JSONException | IOException | InterruptedException | URISyntaxException e) {
             e.printStackTrace();
         }
+
+        return null;
     }
 }
