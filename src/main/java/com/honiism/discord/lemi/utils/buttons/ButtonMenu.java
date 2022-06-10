a8e8c8d79e09351da5ddd36fd11b384a7e144535
illusbyhoppity <honiism@gmail.com>
2022-06-10 16:20:46 +0700

    Modified ButtonMenu

--- src/main/java/com/honiism/discord/lemi/utils/buttons/ButtonMenu.java
+++ src/main/java/com/honiism/discord/lemi/utils/buttons/ButtonMenu.java
@@ -19,14 +19,21 @@
 
 package com.honiism.discord.lemi.utils.buttons;
 
+import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.interactions.components.Modal;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.internal.utils.Checks;
 
 public class ButtonMenu {
 
@@ -34,21 +41,106 @@ public class ButtonMenu {
     private final long timeout;
     private final JDA jda;
     private final Set<Long> allowedUsers;
-    private final List<Button> buttons;
-    private final List<Runnable> actions;
-    private final Runnable finalAction;
-    private final Runnable timeoutAction;
+    private final Map<Button, Runnable> actions;
+    private final Map<Button, Modal> modals;
+    private final Map<Button, Runnable> finalActions;
 
     private ButtonMenu(EventWaiter waiter, long timeout, JDA jda, Set<Long> allowedUsers,
-            List<Button> buttons, List<Runnable> actions, Runnable finalAction, Runnable timeoutAction) {
+            Map<Button, Runnable> actions, Map<Button, Modal> modals, Map<Button, Runnable> finalActions) {
         this.waiter = waiter;
         this.timeout = timeout;
         this.jda = jda;
         this.allowedUsers = allowedUsers;
-        this.buttons = buttons;
         this.actions = actions;
-        this.finalAction = finalAction;
-        this.timeoutAction = timeoutAction;
+        this.modals = modals;
+        this.finalActions = finalActions;
+    }
+
+    public void waitAndModal(Message message, long channelId) {
+        this.waiter.waitForEvent(
+                ButtonInteractionEvent.class,
+                (event) -> {
+                    if (message.getIdLong() != event.getMessageIdLong()) {
+                        return false;
+                    }
+
+                    if (allowedUsers.size() >= 1) {
+                        if (!allowedUsers.contains(event.getMember().getIdLong())) {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                },
+                (event) -> {
+                    actions.get(event.getButton()).run();
+
+                    Button buttonClicked = event.getButton();
+
+                    if (modals.containsKey(buttonClicked)) {
+                        event.replyModal(modals.get(buttonClicked)).queue(
+                            (success) -> {
+                                if (finalActions.containsKey(buttonClicked)) {
+                                    finalActions.get(buttonClicked).run();
+                                }
+                            }
+                        );
+
+                    } else {
+                        if (finalActions.containsKey(buttonClicked)) {
+                            finalActions.get(buttonClicked).run();
+                        }
+                    }
+                },
+                timeout,
+                TimeUnit.SECONDS,
+                () -> {
+                    TextChannel channel = jda.getTextChannelById(channelId);
+                    
+                    if (channel == null) {
+                        return;
+                    }
+
+                    channel.retrieveMessageById(message.getIdLong())
+                        .flatMap(m -> m.editMessageComponents(Collections.emptyList()))
+                        .queue(s -> {}, e -> {});
+                }
+        );
+    }
+
+    public void waitForEvent(Message message, long channelId) {
+        this.waiter.waitForEvent(
+                ButtonInteractionEvent.class,
+                (event) -> {
+                    if (message.getIdLong() != event.getMessageIdLong()) {
+                        return false;
+                    }
+
+                    if (allowedUsers.size() >= 1) {
+                        if (!allowedUsers.contains(event.getMember().getIdLong())) {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                },
+                (event) -> {
+                    actions.get(event.getButton()).run();
+                },
+                timeout,
+                TimeUnit.SECONDS,
+                () -> {
+                    TextChannel channel = jda.getTextChannelById(channelId);
+                    
+                    if (channel == null) {
+                        return;
+                    }
+
+                    channel.retrieveMessageById(message.getIdLong())
+                        .flatMap(m -> m.editMessageComponents(Collections.emptyList()))
+                        .queue(s -> {}, e -> {});
+                }
+        );
     }
 
     public static class Builder {
@@ -57,9 +149,48 @@ public class ButtonMenu {
 
         private EventWaiter waiter;
         private long timeout = -1;
-        private List<Button> buttons;
-        private List<Runnable> actions;
-        private Runnable finalAction;
-        private Runnable timeoutAction;
+        private Map<Button, Runnable> actions;
+        private Map<Button, Modal> modals;
+        private Map<Button, Runnable> finalActions;
+
+        public Builder(JDA jda) {
+            this.jda = jda;
+        }
+
+        public Builder setFinalAction(Map<Button, Runnable> finalActions) {
+            this.finalActions = finalActions;
+            return this;
+        }
+
+        public Builder setModals(Map<Button, Modal> modals) {
+            this.modals = modals;
+            return this;
+        }
+
+        public Builder setTimeout(long timeout, TimeUnit unit) {
+            Checks.notNull(unit, "TimeUnit");
+            Checks.check(timeout > 0, "Timeout must be greater than 0!");
+
+            timeout = unit.toSeconds(timeout);
+            return this;
+        }
+
+        public Builder addAllowedUsers(Long... userIds) {
+            allowedUsers.addAll(Set.of(userIds));
+            return this;
+        }
+
+        public Builder setActions(Map<Button, Runnable> actions) {
+            this.actions = actions;
+            return this;
+        }
+
+        public ButtonMenu build() {
+            Checks.notNull(waiter, "Waiter");
+            Checks.check(timeout != -1, "You must set a timeout using #setTimeout()!");
+            Checks.notNull(actions, "actions");
+
+            return new ButtonMenu(waiter, timeout, jda, allowedUsers, actions, modals, finalActions);
+        }
     }
 }
